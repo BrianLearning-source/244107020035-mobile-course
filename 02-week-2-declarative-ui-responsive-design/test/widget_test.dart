890ab1aad130d26d38c8902daaf42ad5600d8d13
Brian Serafino <briandonovan696@gmail.com>
2026-09-08 05:46:31 +0700

    Lab: simple layout (warm-up)

--- 02-week-2-declarative-ui-responsive-design/test/widget_test.dart
+++ 02-week-2-declarative-ui-responsive-design/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:allweek/main.dart';
+import 'package:allweek/warm_up.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
